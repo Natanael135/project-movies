e6c8009d558d085a393a542d5a3764940ee3c82b
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-08 19:55:25 -0300

    feat: implementando pagina principal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-function App() {
-  return <></>;
-}
+import React, { useState } from "react";
+import Search from "./components/Search";
+
+const App = () => {
+  const [seachTerm, setSeachTerm] = useState("teste");
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="" />
+          <h1>
+            Encontre <span className="text-gradient">Filmes</span> Dos Quais
+            Você Gosta Sem Complicações
+          </h1>
+          <Search seachTerm={seachTerm} setSeachTerm={setSeachTerm} />
+        </header>
+      </div>
+    </main>
+  );
+};
 
 export default App;
